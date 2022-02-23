0a2376a01c9852cde5632db0a0f4767c622e1e8c
unknown <ana.silva-b@mail.escuelaing.edu.co>
2022-02-23 14:46:04 -0500

    Estructura base

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+
+import org.example.server.WeatherServer;
+
+public class App
+{
+    public static void main( String[] args ) {
+        try {
+            WeatherServer.getInstance().startServer(args);
+            } catch (Exception e) {
+            System.out.println(e.getMessage());
+            }
+
+
+    }
+}
---
+++ src/main/java/org/example/server/WeatherServer.java
@@ -0,0 +1,61 @@
+package org.example.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherServer {
+
+        private static WeatherServer _instance = new WeatherServer();
+
+        public String startServer(String[] args) throws IOException {
+
+            StringBuilder result = new StringBuilder();
+            URL url = new URL("https://openweathermap.org/");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                for (String line; (line = reader.readLine()) != null;) {
+                    result.append(line);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Ha ocurrido un error");
+            }
+            System.out.println(result.toString());
+            return result.toString();
+        }
+
+        public static WeatherServer getInstance() {
+            return _instance;
+        }
+
+        public String computeDefaultResponse(String JsonObject){
+            String outputLine =
+                    "HTTP/1.1 200 OK\n"
+                            + "Content-Type: text/html\r\n"
+                            + "\r\n"
+                            + "<!DOCTYPE html>"
+                            + "<html>"
+                            + "<head>"
+                            + "<meta charset=\"UTF-8\">"
+                            + "<title>Title of the document</title>\n"
+                            + "</head>"
+                            + "<body>"
+                            + JsonObject
+                            + "</body>"
+                            + "</html>";
+            return outputLine;
+        }
+
+        static int getPort() {
+            if (System.getenv("PORT") != null) {
+                return Integer.parseInt(System.getenv("PORT"));
+            }
+            return 35000;
+        }
+    }
+
